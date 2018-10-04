f80401b2d7ccf49867000305937ad96deb5c2872
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-10-03 20:43:24 -0700

    javascript file with for loop

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('where am i');
+
+//initial condition
+var countNum = prompt('How high should we count even numbers?')
+
+for(var i = 0; i < countNum;  i++) {
+    if (i % 2 === 0) {
+        console.log(i, 'is even');
+    }
+}
